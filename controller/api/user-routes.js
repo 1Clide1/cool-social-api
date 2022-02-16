be7badbb7ade29a16cf88802aed000e07b5d4bae
Brandon Diaz <brandonjustindiaz@yahoo.com>
2022-02-16 16:45:08 -0500

    haven't tested it but I have added a route to get all users, post a new user, and to delete a user

--- controller/api/user-routes.js
+++ controller/api/user-routes.js
@@ -1,7 +1,40 @@
 // import express router
 const router = require("express").Router();
 // import models
-const { Thoughts, User } = require("../../models");
+const { User, Thoughts } = require("../../models");
 
+// route to get all users
+router.get("/", async (req, res) => {
+  try {
+    const getUsers = await User.find();
+    res.json(getUsers);
+  } catch (err) {
+    res.json(err);
+  }
+});
+// route to post a new user
+router.post("/", async ({ body }, res) => {
+  try {
+    const createUser = await User.create(body);
+    res.json(createUser);
+  } catch (err) {
+    res.json(err);
+  }
+});
+// route to delete a user
+router.delete("/:id", async ({params}, res)=>{
+    try{
+        const deleteUser = await User.findOneAndDelete({ _id: params.id });]\
+        if (!deleteUser){
+            res.json({ message: "there are no users that are found with this id" });
+        } else{
+            res.json(deleteUser);
+            console.log(`user ${deleteUser} has been deleted`);
+        }
+    }
+    catch(err){
+        res.json(err);
+    }
+});
 // export the router
 module.exports = router;
